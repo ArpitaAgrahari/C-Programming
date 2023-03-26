d60a64be7e326b14382a197a89751aeb786ca6c4
arpitaagrahari004@gmail.com <arpitaagrahari004@gmail.com>
2023-03-26 23:23:21 +0530

    edited read me file

---
+++ git7.c
@@ -0,0 +1,9 @@
+// question:  find the ouput of following code (based on increment (pre increment an d post increment of user defined input))
+#include<stdio.h>
+int main( )
+{
+	int a=1,b=1,c;
+	c=a++ +b;
+	printf("\n%d %d %d",a,b,c);
+	return 0;
+}
