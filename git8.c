c5e38d88e003f59cd2767f52db55a6bb64aa2dce
arpitaagrahari004@gmail.com <arpitaagrahari004@gmail.com>
2023-03-26 23:26:07 +0530

    edited read me file

---
+++ git8.c
@@ -0,0 +1,9 @@
+//  question:  find the output of code (use of symbol like greater than or less than , equal to )
+#include<stdio.h>
+int main( )
+{
+	int a=1,b=2,c;
+	c=a>b;
+	printf("\n%d",c);
+	return 0; //output: 0
+}
