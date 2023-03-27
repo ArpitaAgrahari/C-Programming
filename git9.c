44afd169f23183c274f1e0321d6861ef701b58de
arpitaagrahari004@gmail.com <arpitaagrahari004@gmail.com>
2023-03-27 14:30:51 +0530

    edited read me file

---
+++ git9.c
@@ -0,0 +1,10 @@
+// question : find the output of following code ( the use of of == is used to compare lhs to rhs or fopr comparing to numerial input)
+//#include<stdio.h>
+//int main( )
+//{
+//	int a=10,b=5,c=5;
+//	int d;
+//	d=b+c==a;
+//	printf("\n%d",d);
+//	return 0; 
+//}
