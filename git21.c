1b02a9d01726a49606f0528864dfcd968fca97c7
arpitaagrahari004@gmail.com <arpitaagrahari004@gmail.com>
2023-03-30 22:53:26 +0530

    edited read me file

---
+++ git21.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int v;
+	printf("Enter the number : ");
+	scanf("%d",&v);
+	if(v<10)
+	{
+		printf("number is less than 10");
+		}	
+		else
+		{
+			printf("number is greater than 10");
+		}
+		return 0;
+}
