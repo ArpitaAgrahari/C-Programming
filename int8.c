d2d7d60c1efd2c891ebefbd7dbe9d07950aebbd4
Arpita Agrahari <115877354+ArpitaAgrahari@users.noreply.github.com>
2023-04-09 14:17:44 +0530

    Add files via upload

---
+++ int8.c
@@ -0,0 +1,13 @@
+//for(intialization expression;   : intial value 
+//     loop -reption -condition;  : condition of loop 
+//     update expression){         
+//statement;}
+#include <stdio.h>
+int main()
+{
+	int n;
+	for (n=10; n>0; n=n-1){
+		printf("welcome\n");
+	}
+	return 0;
+}
