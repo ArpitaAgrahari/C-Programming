62452717eb152f0b7e94591f08153b0b12f6fd62
Arpita Agrahari <115877354+ArpitaAgrahari@users.noreply.github.com>
2023-04-11 21:10:24 +0530

    Add files via upload

---
+++ int13.c
@@ -0,0 +1,10 @@
+// question :  find the ouput of following code
+#include <stdio.h>      //# output :  in while loop after loop
+int main()
+{
+	do {
+		printf("in while loop\n");
+	} while (0);
+	printf("after loop\n");
+	return 0;
+}
