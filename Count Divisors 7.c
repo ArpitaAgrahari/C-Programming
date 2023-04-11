9de961745c5261b81174c47b08dde4d0f2f66955
Arpita Agrahari <115877354+ArpitaAgrahari@users.noreply.github.com>
2023-04-11 21:40:04 +0530

    Add files via upload

---
+++ Count Divisors 7.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main(){
+    int l,r,k,count = 0;
+    scanf("%d %d %d",&l,&r,&k);
+    for(int i=l;i<=r;i++){
+        if(i % k == 0){
+            count++;
+        }
+    }
+    printf("%d\n",count);
+    return 0;
+}
