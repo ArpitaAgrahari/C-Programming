c544ff25d52f5ee19e678b25a2ce013a64d59bca
Arpita Agrahari <115877354+ArpitaAgrahari@users.noreply.github.com>
2023-04-15 22:56:13 +0530

    Add files via upload

---
+++ int22.c
@@ -0,0 +1,16 @@
+// return  statemnt  : exits the function.
+//  return exits immedaitely from the currently executing function to the calling routine, optionally returning a value.
+// syntax:  return[expression];
+// question  :  return ing the value pf square of user defined function
+# include<stdio.h>  
+int main()
+{
+	int x;
+	printf("enter a number : ");
+	scanf("%d",&x);
+	printf(int sqr (int x);
+{
+		return (x*x);
+	}
+	return 0;
+}
