ddba3ae733dda40746725d4843cda628ad79c7f8
Arpita Agrahari <115877354+ArpitaAgrahari@users.noreply.github.com>
2023-04-18 22:59:22 +0530

    Add files via upload

---
+++ int32.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	float x;
+	printf("Enter the number : ");
+	scanf("%f",&x);
+	x = floor(x);
+	printf("the floor of the number is : %f",x);
+	return 0;	
+}
