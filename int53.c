5ff22f9a1fd9bdce48f4210d964529063252f416
Arpita Agrahari <115877354+ArpitaAgrahari@users.noreply.github.com>
2023-04-29 23:31:02 +0530

    Add files via upload

---
+++ int53.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+	int a[100],n,i;
+	printf("\n Enter number of elements: ");
+	scanf("%d",&n);
+	printf("\n Enter array elements: ");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	printf("\n Entered array element are: ");
+	for(i=0;i<n;i++)
+	{
+		printf("\n%d",a[i]);
+	}
+	return 0;
+}
