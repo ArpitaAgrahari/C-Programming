3f86e60825e8b107703a9d208b17e9d9e0ccc95b
Arpita Agrahari <115877354+ArpitaAgrahari@users.noreply.github.com>
2023-05-05 23:50:54 +0530

    Add files via upload

---
+++ file13.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main(){
+	char *name;// string char array
+	puts("\n Enter  a string : ");
+	gets(name);// to input string with space
+	printf("\nString is: ");
+	puts(name);// to output const string
+	
+}// end main
