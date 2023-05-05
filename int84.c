22bde268a4712dd0f40d3ed62894f09013e272a3
Arpita Agrahari <115877354+ArpitaAgrahari@users.noreply.github.com>
2023-05-05 23:49:14 +0530

    Add files via upload

---
+++ int84.c
@@ -0,0 +1,26 @@
+// program to find sum and mean of 1D array elements using pointer to an array 
+#include <stdio.h>
+int main()
+{
+	int i,n,arr[20],sum = 0;
+	int *pn = &n,*parr=arr,*psum=&sum;
+	float mean = 0.0, *pmean = &mean;
+	printf("\n Enter the elements in the array : ");
+	scanf("%d",pn);
+	for(i=0;i<*pn;i++)
+	{
+		printf("\n Enter the number : ");
+		scanf("%d",(parr+i));
+	}
+	for (i=0;i<*pn;i++)
+	{
+		*psum = *psum+*(arr+i);
+	}
+	*pmean = *psum/ *pn;
+	printf("\n The number you entered are : ");
+	for (i=0;i<*pn;i++)
+	printf("\n %d",(arr+i));
+	printf("\n The sum is %d",*psum);
+	printf("\n The mean is %f",*pmean);
+	return 0;
+}
